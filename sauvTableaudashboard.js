d42cb532082681e4ad67251bcb6051ef22ebbd1c
sarahpgl <sarah.pignol@insa-lyon.fr>
2023-07-27 10:37:15 +0200

    Create sauvTableaudashboard.js

---
+++ sauvTableaudashboard.js
@@ -0,0 +1,663 @@
+// INDEX JRM
+
+
+indexjrm =0;
+for ( i =0; i<data.series[0].fields[0].values.buffer.length; i++){
+  if(data.series[0].fields[0].values.buffer[i]== "fiber_paper"){
+    indexjrm=i;
+    break;
+  }
+}
+totaljrm =data.series[0].fields[2].values.buffer[indexjrm]+data.series[0].fields[4].values.buffer[indexjrm]+data.series[0].fields[6].values.buffer[indexjrm]+data.series[0].fields[8].values.buffer[indexjrm]+data.series[0].fields[10].values.buffer[indexjrm];
+
+
+
+// INDEX EMR
+
+
+indexcartonbrun =0;
+for ( i =0; i<data.series[0].fields[0].values.buffer.length; i++){
+  if(data.series[0].fields[0].values.buffer[i]== "corrugated_cardboard"){
+    indexcartonbrun=i;
+   
+    break;
+  }
+}
+totalcartonbrun =data.series[0].fields[2].values.buffer[indexcartonbrun]+data.series[0].fields[4].values.buffer[indexcartonbrun]+data.series[0].fields[6].values.buffer[indexcartonbrun]+data.series[0].fields[8].values.buffer[indexcartonbrun]+data.series[0].fields[10].values.buffer[indexcartonbrun];
+
+indexcartonnettes =0;
+for ( i =0; i<data.series[0].fields[0].values.buffer.length; i++){
+  if(data.series[0].fields[0].values.buffer[i]== "fiber_boxboard"){
+    indexcartonnettes=i;
+   
+    break;
+  }
+}
+totalcartonnettes =data.series[0].fields[2].values.buffer[indexcartonnettes]+data.series[0].fields[4].values.buffer[indexcartonnettes]+data.series[0].fields[6].values.buffer[indexcartonnettes]+data.series[0].fields[8].values.buffer[indexcartonnettes]+data.series[0].fields[10].values.buffer[indexcartonnettes];
+
+
+
+// INDEX REFUS / FILM
+
+
+indexrefus =[0,0,0,0];
+for ( i =0; i<data.series[0].fields[0].values.buffer.length; i++){
+  if(data.series[0].fields[0].values.buffer[i]== "miscellaneous"){
+    indexrefus[0]=i;
+  }
+  if(data.series[0].fields[0].values.buffer[i]== "plastic_black"){
+    indexrefus[1]=i;
+  }
+  if(data.series[0].fields[0].values.buffer[i]== "organic"){
+    indexrefus[2]=i;
+  }
+  if(data.series[0].fields[0].values.buffer[i]== "glass_container"){
+    indexrefus[3]=i;
+  }
+ 
+}
+
+totalrefus=0
+for (i=0; i<indexrefus.length; i++){
+  totalrefus =data.series[0].fields[2].values.buffer[indexrefus[i]]+data.series[0].fields[4].values.buffer[indexrefus[i]]+data.series[0].fields[6].values.buffer[indexrefus[i]]+data.series[0].fields[8].values.buffer[indexrefus[i]]+data.series[0].fields[10].values.buffer[indexrefus[i]];
+}
+
+
+indexfilm =[0,0];
+for ( i =0; i<data.series[0].fields[0].values.buffer.length; i++){
+  if(data.series[0].fields[0].values.buffer[i]== "snack_packaging" ){
+    indexfilm[0]=i;
+     
+  }
+  if(data.series[0].fields[0].values.buffer[i]== "thin_film" ){
+    indexfilm[1]=i;
+     
+  }
+ 
+}
+
+totalfilm=0
+for (i=0; i<indexfilm.length; i++){
+  totalfilm =data.series[0].fields[2].values.buffer[indexfilm[i]]+data.series[0].fields[4].values.buffer[indexfilm[i]]+data.series[0].fields[6].values.buffer[indexfilm[i]]+data.series[0].fields[8].values.buffer[indexfilm[i]]+data.series[0].fields[10].values.buffer[indexfilm[i]];
+}
+
+
+
+//INDEX PET CLAIR / PEPP
+
+
+indexpppe =[0,0,0,0,0,0,0,0,0];
+for ( i =0; i<data.series[0].fields[0].values.buffer.length; i++){
+  if(data.series[0].fields[0].values.buffer[i]== "plastic_lids" ){
+    indexpppe[0]=i;
+     
+  }
+  if(data.series[0].fields[0].values.buffer[i]== "plastic_caps" ){
+    indexpppe[1]=i;
+     
+  }
+  if(data.series[0].fields[0].values.buffer[i]== "plastic_cup_clear" ){
+    indexpppe[2]=i;
+    
+  }
+  if(data.series[0].fields[0].values.buffer[i]== "blue_pet" ){
+    indexpppe[3]=i;
+    
+  }
+  if(data.series[0].fields[0].values.buffer[i]== "natural_hdpe" ){
+    indexpppe[4]=i;
+    
+  }
+  if(data.series[0].fields[0].values.buffer[i]== "colored_hdpe" ){
+    indexpppe[5]=i;
+    
+  }
+
+  if(data.series[0].fields[0].values.buffer[i]== "plastic_pp_bottle" ){
+    indexpppe[6]=i;
+    
+  }
+
+  if(data.series[0].fields[0].values.buffer[i]== "plastic_pp_pod" ){
+    indexpppe[7]=i;
+    
+  }
+  if(data.series[0].fields[0].values.buffer[i]== "plastic_cups" ){
+    indexpppe[8]=i;
+    
+  }
+}
+totalpppe =0
+for (i=0; i<indexpppe.length; i++){
+    var2 =  data.series[0].fields[2].values.buffer[indexpppe[i]];
+    var4 =  data.series[0].fields[4].values.buffer[indexpppe[i]];
+    var6 =  data.series[0].fields[6].values.buffer[indexpppe[i]];
+    var8 =  data.series[0].fields[8].values.buffer[indexpppe[i]];
+    var10 =  data.series[0].fields[10].values.buffer[indexpppe[i]];
+    if (isNaN(var2) )
+    {
+        var2=0
+    }
+    if (isNaN(var4) )
+    {
+        var4=0
+    }
+    if (isNaN(var6) )
+    {
+        var6=0
+    }
+    if (isNaN(var8) )
+    {
+        var8=0
+    }
+    if (isNaN(var10) )
+    {
+        var10=0
+    }
+    totalpppe += var2+var4+var6+var8+var10;
+     
+  
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+// TOTAUX NOMBRES DE DECHETS PAR MACHINE
+total163 =0
+for (i = 0; i< data.series[0].fields[2].values.buffer.length; i++)
+{
+  if (data.series[0].fields[2].values.buffer[i] != null){
+       total163 +=data.series[0].fields[2].values.buffer[i]
+
+  }
+}
+
+
+ total164 =0
+  for (i = 0; i< data.series[0].fields[6].values.buffer.length; i++)
+  {
+    if (data.series[0].fields[6].values.buffer[i] != null){
+         total164 +=data.series[0].fields[6].values.buffer[i]
+ 
+    }
+  }
+
+ total165 =0
+  for (i = 0; i< data.series[0].fields[8].values.buffer.length; i++)
+  {
+    if (data.series[0].fields[8].values.buffer[i] != null){
+         total165 +=data.series[0].fields[8].values.buffer[i]
+ 
+    }
+  }
+
+ total166 =0
+  for (i = 0; i< data.series[0].fields[10].values.buffer.length; i++)
+  {
+    if (data.series[0].fields[10].values.buffer[i] != null){
+         total166 +=data.series[0].fields[10].values.buffer[i]
+ 
+    }
+  }
+   total167 =0
+  for (i = 0; i< data.series[0].fields[4].values.buffer.length; i++)
+  {
+    if (data.series[0].fields[4].values.buffer[i] != null){
+         total167 +=data.series[0].fields[4].values.buffer[i]
+ 
+    }
+  }
+
+
+// MATIERE PRODUITE
+//JRM
+ htmlgraphicsValue = htmlNode.getElementById('jrm');
+
+if (htmlgraphicsValue) {
+
+  value =data.series[0].fields[13].values.left.buffer[indexjrm]*data.series[0].fields[13].values.right.buffer[indexjrm];
+
+    htmlgraphicsValue.textContent = (value).toPrecision(3);
+ 
+}
+
+//EMR
+htmlgraphicsValue = htmlNode.getElementById('emr');
+
+if (htmlgraphicsValue) {
+   
+    value =data.series[0].fields[14].values.left.buffer[indexcartonbrun]*data.series[0].fields[14].values.right.buffer[indexcartonbrun];
+    value2 =data.series[0].fields[14].values.left.buffer[indexcartonnettes]*data.series[0].fields[14].values.right.buffer[indexcartonnettes];
+
+    htmlgraphicsValue.textContent = (value+value2).toPrecision(3);
+ 
+}
+
+//PET
+htmlgraphicsValue = htmlNode.getElementById('pet');
+
+if (htmlgraphicsValue) {
+ value = 0
+    for ( j = 0; j< indexpppe.length; j++){
+        value+=data.series[0].fields[16].values.left.buffer[indexpppe[j]]*data.series[0].fields[16].values.right.buffer[indexpppe[j]];
+    }
+    
+
+    htmlgraphicsValue.textContent = (value).toPrecision(3);
+}  
+   
+
+// REFUS
+ htmlgraphicsValue = htmlNode.getElementById('refus');
+
+if (htmlgraphicsValue) {
+ value = 0
+    for ( j = 0; j< indexrefus.length; j++){
+        value+=data.series[0].fields[15].values.left.buffer[indexrefus[j]]*data.series[0].fields[15].values.right.buffer[indexrefus[j]];
+    }
+    for ( j = 0; j< indexfilm.length; j++){
+        value+=data.series[0].fields[15].values.left.buffer[indexfilm[j]]*data.series[0].fields[15].values.right.buffer[indexfilm[j]];
+    }
+
+    htmlgraphicsValue.textContent = (value).toPrecision(3);
+  
+   
+}
+
+
+// QUALITE
+//JRM
+ htmlgraphicsValue = htmlNode.getElementById('jrmq');
+
+if (htmlgraphicsValue) {
+  
+  value =data.series[0].fields[2].values.buffer[indexjrm];
+  
+  htmlgraphicsValue.textContent = (value/total163).toPrecision(3);
+ 
+}
+
+//EMR
+ htmlgraphicsValue = htmlNode.getElementById('emrq');
+ if (htmlgraphicsValue) {
+
+  value =data.series[0].fields[6].values.buffer[indexcartonbrun];
+  value2 =data.series[0].fields[6].values.buffer[indexcartonnettes];
+  
+
+
+    htmlgraphicsValue.textContent = ((value+value2)/total164).toPrecision(3);
+
+}
+//PET
+htmlgraphicsValue = htmlNode.getElementById('petq');
+
+if (htmlgraphicsValue) {
+    value =0
+    for ( j = 0; j< indexpppe.length; j++){
+        value += data.series[0].fields[10].values.buffer[indexpppe[j]];
+    }
+    
+    htmlgraphicsValue.textContent = ((value)/total166).toPrecision(3);
+  
+}
+
+
+//REFUS
+ htmlgraphicsValue = htmlNode.getElementById('refusq');
+
+if (htmlgraphicsValue) {
+    value =0
+    for ( j = 0; j< indexrefus.length; j++){
+        value += data.series[0].fields[8].values.buffer[indexrefus[j]];
+    }
+    for ( j = 0; j< indexfilm.length; j++){
+        value += data.series[0].fields[8].values.buffer[indexfilm[j]];
+    } 
+
+ 
+
+    htmlgraphicsValue.textContent = ((value)/total165).toPrecision(3);
+  
+}
+
+
+
+
+//TAUX DE CAPTATION
+//JRM
+ htmlgraphicsValue = htmlNode.getElementById('jrmt');
+
+if (htmlgraphicsValue) {
+    value =0
+    for ( j = 0; j< indexrefus.length; j++){
+        value += data.series[0].fields[2].values.buffer[indexrefus[j]];
+    }  
+
+    htmlgraphicsValue.textContent =(total163/(total163+ value)).toPrecision(3);
+
+}
+
+
+//EMR
+ htmlgraphicsValue = htmlNode.getElementById('emrt');
+
+if (htmlgraphicsValue) {
+   
+    value =0
+    for ( j = 0; j< indexrefus.length; j++){
+        value += data.series[0].fields[6].values.buffer[indexrefus[j]];
+    }  
+
+    htmlgraphicsValue.textContent =(total164/(total164+ value)).toPrecision(3);
+ 
+}
+//PET
+
+
+htmlgraphicsValue = htmlNode.getElementById('pett');
+
+if (htmlgraphicsValue) {
+  
+    value =0
+    for ( j = 0; j< indexpppe.length; j++){
+        value += data.series[0].fields[10].values.buffer[indexpppe[j]];
+    }  
+
+    htmlgraphicsValue.textContent =(total166/(total166+ value)).toPrecision(3);
+}
+
+//REFUS
+
+ htmlgraphicsValue = htmlNode.getElementById('refust');
+
+if (htmlgraphicsValue) {
+  
+    value =0
+    for ( j = 0; j< indexrefus.length; j++){
+        value += data.series[0].fields[8].values.buffer[indexrefus[j]];
+    }  
+
+    htmlgraphicsValue.textContent =(total165/(total165+ value)).toPrecision(3);
+}
+
+
+
+//DIFFUSION
+//JRM
+ htmlgraphicsValue = htmlNode.getElementById('jrmd');
+
+if (htmlgraphicsValue) {
+  
+  valuejrm1 =data.series[0].fields[2].values.buffer[indexjrm];
+  diffusionjrm1 =((valuejrm1/totaljrm)*100).toPrecision(3)
+  
+  
+
+    htmlgraphicsValue.textContent =diffusionjrm1;
+
+}
+
+
+
+ htmlgraphicsValue = htmlNode.getElementById('jrmd2');
+
+if (htmlgraphicsValue) {
+   
+  valuejrm2 =data.series[0].fields[6].values.buffer[indexjrm];
+  diffusionjrm2 = (valuejrm2*100/totaljrm).toPrecision(3);
+  
+    htmlgraphicsValue.textContent = diffusionjrm2;
+  
+ 
+}
+
+
+ htmlgraphicsValue = htmlNode.getElementById('jrmd3');
+
+if (htmlgraphicsValue) {
+  
+  valuejrm3 =data.series[0].fields[10].values.buffer[indexjrm];
+  diffusionjrm3 =(valuejrm3*100/totaljrm).toPrecision(3);
+ 
+
+    htmlgraphicsValue.textContent =diffusionjrm3;
+
+}
+
+
+ htmlgraphicsValue = htmlNode.getElementById('jrmd4');
+
+if (htmlgraphicsValue) {
+  
+  valuejrm4 =data.series[0].fields[4].values.buffer[indexjrm];
+  diffusionjrm4 = (valuejrm4*100/totaljrm).toPrecision(3);
+  
+
+    htmlgraphicsValue.textContent =diffusionjrm4;
+ 
+}
+
+ htmlgraphicsValue = htmlNode.getElementById('jrmd5');
+
+if (htmlgraphicsValue) {
+  
+  valuejrm5 =data.series[0].fields[8].values.buffer[indexjrm];
+  diffusionjrm5 = (valuejrm5*100/totaljrm).toPrecision(3);
+
+    htmlgraphicsValue.textContent = diffusionjrm5;
+
+}
+
+ htmlgraphicsValue = htmlNode.getElementById('total1');
+
+if (htmlgraphicsValue) {
+  
+  
+    htmlgraphicsValue.textContent = (parseFloat(diffusionjrm1)+parseFloat(diffusionjrm2)+parseFloat(diffusionjrm3)+parseFloat(diffusionjrm4)+parseFloat(diffusionjrm5)).toPrecision(4);
+ 
+}
+
+//EMR
+ htmlgraphicsValue = htmlNode.getElementById('emrd');
+
+if (htmlgraphicsValue) {
+   
+ 
+  valueemr1 =data.series[0].fields[2].values.buffer[indexcartonbrun];
+ 
+  valueemr1_2 =data.series[0].fields[2].values.buffer[indexcartonnettes];
+  
+
+  diffusionemr1 =(((valueemr1+valueemr1_2)/(totalcartonbrun+totalcartonnettes)*100)).toPrecision(3)
+  
+  
+
+    htmlgraphicsValue.textContent =diffusionemr1;
+
+}
+
+ htmlgraphicsValue = htmlNode.getElementById('emrd2');
+
+if (htmlgraphicsValue) {
+
+ 
+  valueemr2 =data.series[0].fields[6].values.buffer[indexcartonbrun];
+  valueemr2_2 =data.series[0].fields[6].values.buffer[indexcartonnettes];
+ 
+  diffusionemr2 = (((valueemr2+valueemr2_2)/(totalcartonbrun+totalcartonnettes)*100)).toPrecision(3)
+  
+  htmlgraphicsValue.textContent = diffusionemr2;
+  
+}
+
+
+ htmlgraphicsValue = htmlNode.getElementById('emrd3');
+
+if (htmlgraphicsValue) {
+
+ 
+  valueemr3 =data.series[0].fields[10].values.buffer[indexcartonbrun];
+  valueemr3_2 =data.series[0].fields[10].values.buffer[indexcartonnettes];
+ 
+  diffusionemr3 = (((valueemr3+valueemr3_2)/(totalcartonbrun+totalcartonnettes)*100)).toPrecision(3)
+  
+  
+
+    htmlgraphicsValue.textContent = diffusionemr3;
+  
+}
+
+
+ htmlgraphicsValue = htmlNode.getElementById('emrd4');
+
+if (htmlgraphicsValue) {
+
+ 
+  valueemr4 =data.series[0].fields[4].values.buffer[indexcartonbrun];
+  valueemr4_2 =data.series[0].fields[4].values.buffer[indexcartonnettes];
+ 
+  diffusionemr4 = (((valueemr4+valueemr4_2)/(totalcartonbrun+totalcartonnettes)*100)).toPrecision(3)
+  
+
+
+  htmlgraphicsValue.textContent = diffusionemr4;
+
+}
+
+
+ htmlgraphicsValue = htmlNode.getElementById('emrd5');
+
+if (htmlgraphicsValue) {
+
+ 
+  valueemr5 =data.series[0].fields[8].values.buffer[indexcartonbrun];
+  valueemr5_2 =data.series[0].fields[8].values.buffer[indexcartonnettes];
+ 
+  diffusionemr5 = (((valueemr5+valueemr5_2)/(totalcartonbrun+totalcartonnettes)*100)).toPrecision(3)
+  
+  
+    htmlgraphicsValue.textContent = diffusionemr5;
+ 
+  
+}
+
+ htmlgraphicsValue = htmlNode.getElementById('total2');
+
+if (htmlgraphicsValue) {
+
+    htmlgraphicsValue.textContent = (parseFloat(diffusionemr1)+parseFloat(diffusionemr2)+parseFloat(diffusionemr3)+parseFloat(diffusionemr4)+parseFloat(diffusionemr5)).toPrecision(4);
+  
+}
+
+//PET
+htmlgraphicsValue = htmlNode.getElementById('petd');
+
+if (htmlgraphicsValue) {
+    valuepet1 =0
+  for(i =0; i<indexpppe.length; i++){
+    valuepet1 +=data.series[0].fields[2].values.buffer[indexpppe[i]];
+    
+  }
+
+  
+  diffusionpet1 =((valuepet1/totalpppe)*100).toPrecision(3)
+  
+  
+
+    htmlgraphicsValue.textContent =diffusionpet1;
+
+}
+
+
+
+ htmlgraphicsValue = htmlNode.getElementById('petd2');
+
+ if (htmlgraphicsValue) {
+    valuepet2 =0
+  for(i =0; i<indexpppe.length; i++){
+    
+    valuepet2 +=data.series[0].fields[6].values.buffer[indexpppe[i]];
+    
+  }
+  
+  diffusionpet2 =((valuepet2/totalpppe)*100).toPrecision(3)
+  
+  
+
+    htmlgraphicsValue.textContent =diffusionpet2;
+ }
+
+ htmlgraphicsValue = htmlNode.getElementById('petd3');
+
+ if (htmlgraphicsValue) {
+    valuepet3 =0
+  for(i =0; i<indexpppe.length; i++){
+    valuepet3 +=data.series[0].fields[10].values.buffer[indexpppe[i]];
+
+  }
+  
+  diffusionpet3 =((valuepet3/totalpppe)*100).toPrecision(3)
+  
+  
+
+    htmlgraphicsValue.textContent =diffusionpet3;
+ }
+
+
+ htmlgraphicsValue = htmlNode.getElementById('petd4');
+
+
+if (htmlgraphicsValue) {
+    valuepet4 =0
+    for(i =0; i<indexpppe.length; i++){
+    valuepet4 +=data.series[0].fields[4].values.buffer[indexpppe[i]];
+      
+    }
+    
+    diffusionpet4 =((valuepet4/totalpppe)*100).toPrecision(3)
+    
+    
+
+    htmlgraphicsValue.textContent =diffusionpet4;
+    }
+    
+
+ htmlgraphicsValue = htmlNode.getElementById('petd5');
+
+ if (htmlgraphicsValue) {
+    valuepet5 =0
+  for(i =0; i<indexpppe.length; i++){
+    vari =data.series[0].fields[8].values.buffer[indexpppe[i]];
+    if (isNaN(vari)==0){
+    valuepet5 +=vari;
+    }
+    
+  }
+  
+  diffusionpet5 =((valuepet5/totalpppe)*100).toPrecision(3)
+  
+  
+
+    htmlgraphicsValue.textContent =diffusionpet5;
+ }
+
+
+ htmlgraphicsValue = htmlNode.getElementById('total3');
+
+if (htmlgraphicsValue) {
+  
+  
+    htmlgraphicsValue.textContent = (parseFloat(diffusionpet1)+parseFloat(diffusionpet2)+parseFloat(diffusionpet3)+parseFloat(diffusionpet4)+parseFloat(diffusionpet5)).toPrecision(4);
+ 
+}
+value =data.series
+console.log(value)
